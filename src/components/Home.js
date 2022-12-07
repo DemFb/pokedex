6f2e6c176c378c77d697b9eec585a7a1557a0a4f
D.Fab <dembele.fabala@gmail.com>
2022-12-07 10:28:15 +0100

    feat: add project structure + home page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Sign up </Text>
+      <View>
+        <Text>Page d'accueil</Text>
+      </View>
+    </View>
+  );
+}
+
+export default Home
+
+const styles = StyleSheet.create({});

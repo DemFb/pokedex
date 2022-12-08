3371fdf52512bce33b314e97a1831fe684e611ce
Nicolas9578 <93204465+NicolasOlivier@users.noreply.github.com>
2022-12-08 16:07:02 +0100

    refactor code

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,27 @@
 // import SearchBar from 'react-native-dynamic-search-bar';
-// import React, {useEffect, useState} from 'react';
-// import {fetchData} from '../api/apiHelper';
+// import React, {useMemo, useState} from 'react';
+// import {data} from './Home';
 
 // export const BarDeRecherche = props => {
-//   const [searchQuery, setSearchQuery] = useState();
+//   const [text, setText] = useState('');
+
+//   const datafilter = useMemo(() => {
+//     return data
+//       .filter(function (item) {
+//         return item.name.includes(text);
+//       })
+//       .map(function ({id, name, image}) {
+//         return {id, name, image};
+//       });
+//   }, [text]);
 
 //   return (
 //     <SearchBar
 //       style={{marginTop: 50}}
 //       placeholder="Search here"
 //       onPress={() => clearImmediate}
-//       onChangeText={setSearchQuery}
+//       onChangeText={newText => setText(newText)}
+//       value={text}
 //     />
 //   );
 // };
